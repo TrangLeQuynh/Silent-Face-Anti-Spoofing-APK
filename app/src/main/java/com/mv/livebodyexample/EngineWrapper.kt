871063212ddf33b332665007cdc5f785971a9871
Trang Le <6527627-TrangLeQuynh@users.noreply.gitlab.com>
2022-03-21 14:16:57 +0700

    [Face-Detection] Remove load model and detect function of FAS

--- app/src/main/java/com/mv/livebodyexample/EngineWrapper.kt
+++ app/src/main/java/com/mv/livebodyexample/EngineWrapper.kt
@@ -3,39 +3,40 @@ package com.mv.livebodyexample
 import android.content.res.AssetManager
 import com.mv.engine.FaceBox
 import com.mv.engine.FaceDetector
-import com.mv.engine.Live
+//import com.mv.engine.Live
 
 
 class EngineWrapper(private var assetManager: AssetManager) {
 
     private var faceDetector: FaceDetector = FaceDetector()
-    private var live: Live = Live()
+//    private var live: Live = Live()
 
     fun init(): Boolean {
         var ret = faceDetector.loadModel(assetManager)
-        if (ret == 0) {
-            ret = live.loadModel(assetManager)
-            return ret == 0
-        }
-
-        return false
+//        if (ret == 0) {
+//            ret = live.loadModel(assetManager)
+//            return ret == 0
+//        }
+//        return false
+        return ret == 0
     }
 
     fun destroy() {
         faceDetector.destroy()
-        live.destroy()
+//        live.destroy()
     }
 
     fun detect(yuv: ByteArray, width: Int, height: Int, orientation: Int): DetectionResult {
+        val begin = System.currentTimeMillis()
         val boxes = detectFace(yuv, width, height, orientation)
         if (boxes.isNotEmpty()) {
-            val begin = System.currentTimeMillis()
-            val box = boxes[0].apply {
-                val c = detectLive(yuv, width, height, orientation, this)
-                confidence = c
-            }
-            val end = System.currentTimeMillis()
-            return DetectionResult(box, end - begin, true)
+//            val box = boxes[0].apply {
+//                val c = detectLive(yuv, width, height, orientation, this)
+//                confidence = c
+//            }
+//            val end = System.currentTimeMillis()
+//            return DetectionResult(box, end - begin, true)
+            return DetectionResult(boxes[0], System.currentTimeMillis() - begin, true)
         }
 
         return DetectionResult()
@@ -48,12 +49,12 @@ class EngineWrapper(private var assetManager: AssetManager) {
         orientation: Int
     ): List<FaceBox> = faceDetector.detect(yuv, width, height, orientation)
 
-    private fun detectLive(
-        yuv: ByteArray,
-        width: Int,
-        height: Int,
-        orientation: Int,
-        faceBox: FaceBox
-    ): Float = live.detect(yuv, width, height, orientation, faceBox)
+//    private fun detectLive(
+//        yuv: ByteArray,
+//        width: Int,
+//        height: Int,
+//        orientation: Int,
+//        faceBox: FaceBox
+//    ): Float = live.detect(yuv, width, height, orientation, faceBox)
 
 }
